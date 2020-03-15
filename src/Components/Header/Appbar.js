fe296cbf976277d1c3065297081de9d1a2cb4ce6
Danny393 <danielacosta393@gmail.com>
2020-03-14 23:01:44 -0400

    Set the nav bar ready for redirect, done till theme is chosen

--- src/Components/Header/Appbar.js
+++ src/Components/Header/Appbar.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { fade, makeStyles } from '@material-ui/core/styles';
-import {AppBar, Toolbar, Typography, InputBase} from '@material-ui/core'
+import {AppBar, Toolbar, Typography, InputBase, Button, IconButton} from '@material-ui/core'
 import SearchIcon from '@material-ui/icons/Search';
 import Container from '@material-ui/core/Container/'
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
 
+//colors can be changed, Store Logo can be be a button that redirects to Landing Page
+//number for cart can be updated dynamically 
 const useStyles = makeStyles(theme => ({
   grow: {
     flexGrow: 1,
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
+  navButton: {
+    margin: theme.spacing(1),
+    color: 'white',
   },
   title: {
     display: 'none',
@@ -33,19 +37,15 @@ const useStyles = makeStyles(theme => ({
     },
   },
   searchIcon: {
-    width: theme.spacing(7),
     height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    color: 'white',
+    position: "relative"
   },
   inputRoot: {
     color: 'inherit',
   },
   inputInput: {
-    padding: theme.spacing(1, 1, 1, 7),
+    padding: theme.spacing(1, 0, 1, 2),
     transition: theme.transitions.create('width'),
     width: '100%',
     [theme.breakpoints.up('md')]: {
@@ -62,28 +62,41 @@ export default function PrimarySearchAppBar() {
 
   return (
     <div className={classes.grow}>
-        <Container>
-      <AppBar position="fixed" className = {classes.color}>
-        <Toolbar>
-          <Typography className={classes.title} variant="h6" noWrap>
-            Home
-          </Typography>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </div>
-          <div className={classes.grow} />
-        </Toolbar>
-      </AppBar>
+      <Container>
+        <AppBar position="fixed" className = {classes.color}>
+          <Toolbar>
+            <Typography className={classes.title} variant="h6" noWrap>
+              Store Logo
+            </Typography>
+              <div className={classes.search}>
+                <InputBase
+                  placeholder="Search…"
+                  classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                  }}
+                  inputProps={{ 'aria-label': 'search' }}
+                />
+                <IconButton className = {classes.searchIcon} size = "small">
+                  <SearchIcon />
+                </IconButton>
+                </div>
+              <div className={classes.grow} />
+                <Button className={classes.navButton}>
+                  Products
+                </Button>
+                <Button className = {classes.navButton}>
+                  About Us
+                </Button>
+                <Button className = {classes.navButton}>
+                  Account
+                </Button>
+                <Button className = {classes.navButton}> 
+                  <AddShoppingCartIcon/>
+                  (0)
+                </Button>
+          </Toolbar>
+        </AppBar>
       </Container>
     </div>
   );
